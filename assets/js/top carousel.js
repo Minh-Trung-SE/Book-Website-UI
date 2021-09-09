6a7863543c9131bbdbc0454f4d181e006a243c3d
Minh-Trung-SE <nguyenminhtrungse@gmail.com>
2021-09-09 22:38:09 +0700

    fix bug js

--- assets/js/top carousel.js
+++ assets/js/top carousel.js
@@ -1,42 +1,79 @@
-// $ = document.querySelector.bind(document);
-// $$ = document.querySelectorAll.bind(document);
+$ = document.querySelector.bind(document);
+$$ = document.querySelectorAll.bind(document);
 
-// let indexSlideActive = 0
-// let slides = $$('.top-carousel-item')
-// let dots = $$('.top-carousel-dot')
+let indexSlideActive = 0
+let slides = $$('.top-carousel-item')
+let dots = $$('.top-carousel-dot')
+
+const handleNextSlide = function(currentIndex) {
+    $('.top-carousel-item--active').classList.remove('top-carousel-item--active', 'faceIn', 'faceOut')
+    $('.top-carousel-dot--active').classList.remove('top-carousel-dot--active')
+    if (currentIndex == undefined) {
+        indexSlideActive++
+    } else {
+        indexSlideActive = currentIndex
+    }
+    if (indexSlideActive >= slides.length || indexSlideActive < 0) {
+        indexSlideActive = 0
+    }
+    console.log(indexSlideActive)
+    slides[indexSlideActive].classList.add('top-carousel-item--active', 'faceIn')
+    dots[indexSlideActive].classList.add('top-carousel-dot--active')
+}
+
+const handlePreviousSlide = function(currentIndex) {
+
+    console.log(currentIndex)
+    $('.top-carousel-item--active').classList.remove('top-carousel-item--active', 'faceIn', 'faceOut')
+    $('.top-carousel-dot--active').classList.remove('top-carousel-dot--active')
+    if (currentIndex == undefined) {
+        indexSlideActive--
+    } else {
+        indexSlideActive = currentIndex
+    }
+    if (indexSlideActive >= slides.length || indexSlideActive < 0) {
+        indexSlideActive = slides.length - 1
+    }
+    slides[indexSlideActive].classList.add('top-carousel-item--active', 'faceOut')
+    dots[indexSlideActive].classList.add('top-carousel-dot--active')
+}
+
+let intervalNextSlide = setInterval(handleNextSlide, 3000)
 
 
-// const handleNextSlide = function(nextIndexSlide) {
-//     $('.top-carousel-item--active').classList.remove('top-carousel-item--active')
-//     $('.top-carousel-dot--active').classList.remove('top-carousel-dot--active')
-//     console.log(nextIndexSlide)
-//     if (nextIndexSlide == undefined) {
-//         indexSlideActive++
-//         console.log(nextIndexSlide)
+dots.forEach(function(dot, index) {
+    dot.onclick = function() {
+        clearInterval(intervalNextSlide)
+            // Clear and stop funtion intervalNextSlide
+            //Get index slide to next
+        indexSlideActive = index
+        handleNextSlide(indexSlideActive)
+            // Recall intervalNextSlide
+        intervalNextSlide = setInterval(handleNextSlide, 8000)
+    }
+});
 
-//     }
-//     if (indexSlideActive === slides.length || indexSlideActive < 0) {
-//         indexSlideActive = 0
-//     }
-//     slides[indexSlideActive].classList.add('top-carousel-item--active')
-//     dots[indexSlideActive].classList.add('top-carousel-dot--active')
-// }
-
-// let intervalNextSlide = setInterval(handleNextSlide, 3000)
-
-
-// dots.forEach(function(dot, index) {
-//     dot.onclick = function() {
-//         clearInterval(intervalNextSlide)
-//             // Clear and stop funtion intervalNextSlide
-//             //Get index slide to next
-//         indexSlideActive = index
-//         handleNextSlide(indexSlideActive)
-//             // Recall intervalNextSlide
-//         intervalNextSlide = setInterval(handleNextSlide, 3000)
-//     }
-// });
-
-// $('.top-carousel-list').slick({
-
-// });
+slides.forEach(function(slide, index) {
+    slide.onmousedown = function(e) {
+        var currentIndex = index
+        console.log("Current idex slide: ", index)
+        let xFirst = e.pageX;
+        // console.log("xFirst ", xFirst)
+        slide.onmouseup = function(e) {
+            // console.log("xLast ", e.pageX)
+            let xChange = e.pageX - xFirst;
+            if (xChange > 0) {
+                clearInterval(intervalNextSlide)
+                handlePreviousSlide(currentIndex - 1)
+                console.log("Back")
+                intervalNextSlide = setInterval(handleNextSlide, 3000)
+            } else if (xChange < 0) {
+                clearInterval(intervalNextSlide)
+                handleNextSlide(currentIndex + 1)
+                console.log("Next")
+                intervalNextSlide = setInterval(handleNextSlide, 3000)
+            }
+            console.log(xChange)
+        }
+    }
+})
