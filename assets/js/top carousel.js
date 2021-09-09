1783c003b9b0dd5772ebb9ccfa704157dd00f1ef
Minh-Trung-SE <nguyenminhtrungse@gmail.com>
2021-09-09 22:48:55 +0700

    DONE slide show

--- assets/js/top carousel.js
+++ assets/js/top carousel.js
@@ -16,14 +16,11 @@ const handleNextSlide = function(currentIndex) {
     if (indexSlideActive >= slides.length || indexSlideActive < 0) {
         indexSlideActive = 0
     }
-    console.log(indexSlideActive)
     slides[indexSlideActive].classList.add('top-carousel-item--active', 'faceIn')
     dots[indexSlideActive].classList.add('top-carousel-dot--active')
 }
 
 const handlePreviousSlide = function(currentIndex) {
-
-    console.log(currentIndex)
     $('.top-carousel-item--active').classList.remove('top-carousel-item--active', 'faceIn', 'faceOut')
     $('.top-carousel-dot--active').classList.remove('top-carousel-dot--active')
     if (currentIndex == undefined) {
@@ -56,24 +53,18 @@ dots.forEach(function(dot, index) {
 slides.forEach(function(slide, index) {
     slide.onmousedown = function(e) {
         var currentIndex = index
-        console.log("Current idex slide: ", index)
         let xFirst = e.pageX;
-        // console.log("xFirst ", xFirst)
         slide.onmouseup = function(e) {
-            // console.log("xLast ", e.pageX)
             let xChange = e.pageX - xFirst;
             if (xChange > 0) {
                 clearInterval(intervalNextSlide)
                 handlePreviousSlide(currentIndex - 1)
-                console.log("Back")
                 intervalNextSlide = setInterval(handleNextSlide, 3000)
             } else if (xChange < 0) {
                 clearInterval(intervalNextSlide)
                 handleNextSlide(currentIndex + 1)
-                console.log("Next")
                 intervalNextSlide = setInterval(handleNextSlide, 3000)
             }
-            console.log(xChange)
         }
     }
 })
